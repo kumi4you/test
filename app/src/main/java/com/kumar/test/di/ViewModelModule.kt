d070c5febedc45c2d2c275d931bad4f4f46b44d1
Kumar Saurabh <kumar.saurabh@zelfi.com>
2021-04-13 21:31:14 +0200

    getting data in viewmodel

--- app/src/main/java/com/kumar/test/di/ViewModelModule.kt
+++ app/src/main/java/com/kumar/test/di/ViewModelModule.kt
@@ -1,7 +1,9 @@
 package com.kumar.test.di
 
+import com.kumar.test.presentation.viewmodel.UserViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+    viewModel { UserViewModel(get()) }
 }
--- app/src/main/java/com/kumar/test/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/kumar/test/presentation/viewmodel/UserViewModel.kt
@@ -1,7 +1,38 @@
 package com.kumar.test.presentation.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kumar.test.data.model.UserResponse
 import com.kumar.test.domain.usecase.GetUsersUseCase
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class UserViewModel(private val getUsersUseCase: GetUsersUseCase) {
+class UserViewModel(private val getUsersUseCase: GetUsersUseCase) : ViewModel() {
+
+    private val _userList = MutableLiveData<List<UserResponse>>()
+    val userList: LiveData<List<UserResponse>> get() = _userList
+
+    private val errorHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        onError(throwable)
+    }
+
+    private fun getUserList() {
+        viewModelScope.launch(errorHandler) {
+            getUsersUseCase.execute().collect {
+                _userList.postValue(it)
+            }
+        }
+    }
+
+
+    private fun onError(throwable: Throwable) {
+        Log.e("Error", "Error getting the user list", throwable)
+    }
+
 
 }
+
